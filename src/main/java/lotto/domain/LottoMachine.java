9842125eb78f6b6a2fabfa4218c72d72729f539b
chanYong <yong113012@naver.com>
2025-11-03 03:15:22 +0900

    refactor: 스트림 toList() 메서드 사용하여 LottoMachine 코드 간결화

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,18 +1,14 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
 
     private static final int LOTTO_PRICE = 1000;
 
-    public List<Lotto> generateLottos(List<List<Integer>> inputNumbersList) {
-        List<Lotto> lottos = new ArrayList<>();
-        for (List<Integer> numbers : inputNumbersList) {
-            Lotto lotto = new Lotto(numbers);
-            lottos.add(lotto);
-        }
-        return lottos;
+    public List<Lotto> createLottosFromNumbers(List<List<Integer>> inputNumbersList) {
+        return inputNumbersList.stream()
+                .map(Lotto::new)
+                .toList();
     }
 }
