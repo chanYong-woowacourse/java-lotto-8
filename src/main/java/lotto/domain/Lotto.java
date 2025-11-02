88002b03ef9b31d9abf18a5bc7e058964f145505
chanYong <yong113012@naver.com>
2025-11-03 01:46:03 +0900

    feat: 로또 번호 중복 및 범위 검증 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,7 +16,24 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        if (numbers == null) {
+            throw new IllegalArgumentException("로또 번호는 null일 수 없습니다.");
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers); //중복 허용하지 않는 자료구조
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호에 중복이 있으면 안 됩니다.");
+        }
+
+        for (int num : numbers) {
+            if (num < 1 || num > 45) {
+                throw new IllegalArgumentException("로또 번호는 1~45 범위여야 합니다.");
+            }
+        }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
