0c6fb71c0acdba729bab048b57a350b3ecafcfef
chanYong <yong113012@naver.com>
2025-11-03 22:49:16 +0900

    feat: 당첨 번호와 보너스 번호 입력 검증 로직 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,27 +1,50 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.util.Validator;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class InputView {
     public static int readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        while (true) {
+            try {
+                String input = Console.readLine();
+                Validator.validatePurchaseAmount(input);
+                return Integer.parseInt(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public static List<Integer> readWinningNumbers() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
-        return Stream.of(Console.readLine().split(","))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        while (true) {
+            try {
+                String input = Console.readLine();
+                return Arrays.stream(input.split(","))
+                        .map(String::trim)
+                        .map(Integer::parseInt)
+                        .toList();
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 당첨 번호는 숫자여야 합니다.");
+            }
+        }
     }
 
     public static int readBonusNumber() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        while (true) {
+            try {
+                return Integer.parseInt(Console.readLine());
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 보너스 번호는 숫자여야 합니다.");
+            }
+        }
     }
 }
