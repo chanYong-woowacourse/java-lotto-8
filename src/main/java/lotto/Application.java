7143aa1cc3ca7f56cd68190d27fefea723ea6524
chanYong <yong113012@naver.com>
2025-11-18 23:44:34 +0900

    refactor: main 입력 검증 오류 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -14,8 +14,8 @@ public class Application {
         List<Lotto> lottos = lottoMachine.createLottos(purchaseAmount);
         OutputView.printLottos(lottos);
 
-        List<Integer> winningNumbers = readValidWinningNumbers();
-        int bonusNumber = readValidBonusNumber(winningNumbers);
+        List<Integer> winningNumbers = InputView.readWinningNumbers();
+        int bonusNumber = InputView.readBonusNumber(winningNumbers);
 
         WinningLotto winningLotto = new WinningLotto(new Lotto(winningNumbers), bonusNumber);
 
@@ -25,32 +25,5 @@ public class Application {
 
         LottoResult result = new LottoResult(ranks);
         OutputView.printResult(result, purchaseAmount);
-    }
-
-    private static List<Integer> readValidWinningNumbers() {
-        while (true) {
-            try {
-                List<Integer> numbers = InputView.readWinningNumbers();
-                new Lotto(numbers);
-                return numbers;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-    }
-
-    private static int readValidBonusNumber(List<Integer> winningNumbers) {
-        while (true) {
-            try {
-                int bonusNumber = InputView.readBonusNumber(winningNumbers);
-                if (winningNumbers.contains(bonusNumber)) {
-                    throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
-                }
-                new Lotto(List.of(1, 2, 3, 4, 5, bonusNumber));  // 범위 검증
-                return bonusNumber;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
     }
 }
